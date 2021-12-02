83ea9680e1971cbcdd134175b1e0b2be604dc09a
cincu4221 <75069917+cincu4221@users.noreply.github.com>
2021-12-02 17:16:12 +0900

    make it better

--- app.py
+++ app.py
@@ -1,29 +1,99 @@
-import os
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+import numpy as np
+from dash.dependencies import Output, Input
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+data = pd.read_csv("data/avocado.csv", index_col=0)
 
+data["Date"] = pd.to_date time(data["Date"], format="%Y-%m-%d")
+data.sort_values("Date", inplace=True)
+
+print(data[['region', 'type', 'Date']].head())
+
+external_stylesheets = [
+    {
+        "href": "https://fonts.googleapis.com/css2?"
+                "family=Lato:wght@400;700&display=swap",
+        "rel": "stylesheet",
+    },
+]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+app.title = "Temp Analytics: Understand Your Data!"
 server = app.server
 
-app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
-    ),
-    html.Div(id='display-value')
-])
+@app.callback(
+    [Output("price-chart", "figure"), Output("volume-chart", "figure")],
+    [
+        Input("region-filter", "value"),
+        Input("type-filter", "value"),
+        Input("date-range", "start_date"),
+        Input("date-range", "end_date")
+    ],
+)
+def update_charts(region, avocado_ type, start_date, end_date):
+    mask = (
+        (data. region == region)
+        & (data.type == avocado_type)
+        & (data.Date >= start_date)
+        & (data.Date <= end_date)
+    )
+    filtered_data = data.loc[mask, :]
+    price_chart_figure = {
+        "data": [
+            {
+                "x": filtered_data["Date"],
+                "y": filtered_data["AveragePrice"],
+                "type": "lines",
+                "hovertemplate": "$%{y:.2f}<extra></extra>",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "Average Price of Avocados",
+                "x": 0.05,
+                "xanchor": "left",
+            },
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"tickprefix": "$", "fixedrange": True},
+            "colorway": ["#17B897"],
+        },
+    }
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-                [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+    volume_chart_figure = {
+        "data": [
+            {
+                "x": filtered_data["Date"],
+                "y": filtered_data["Total Volume"],
+                "type": "lines",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "Avocados Sold",
+                "x": 0.05,
+                "xanchor": "left"
+            },
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"fixedrange": True},
+            "colorway": ["#E12D39"],
+        },
+    }
+    return price_chart_figure, volume_chart_figure
 
-if __name__ == '__main__':
+app.layout = html.Div(
+    childern=[
+        html.div(
+            children=[
+                html.P(children="📈", className="header_emoji"),
+                html.H1(children="temp Analytics", className="header_title"),
+                html.P(children="Temp", className="header_description")
+            ],
+            className='header'
+        ),
+    ]
+)
+
+if __name__ == "__main__":
     app.run_server(debug=True)
