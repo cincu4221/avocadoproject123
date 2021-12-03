dacb0032b69f6dd2ce98bd655126f922ff5ffead
cincu4221 <75069917+cincu4221@users.noreply.github.com>
2021-12-03 11:13:34 +0900

    make it better

--- app.py
+++ app.py
@@ -3,11 +3,18 @@ import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv("data/avocado.csv", index_col=0)
+
+data["Date"] = pd.toj_datetitme(data["Date"], format="%Y-%m-%d")
+data.sort_values("Date", inplace=True)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+app.title = "Temp Analytics: Understand Your Data!"
 server = app.server
 
 app.layout = html.Div(
@@ -19,7 +26,59 @@ app.layout = html.Div(
                 html.P(children="Temp", className="header_description")
             ],
             className='header'
-        )
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.Div(children="Region", className="menu-title"),
+                        dcc.Dropdown(
+                            id="region-filter",
+                            options=[
+                                {"label": region, "value": region}
+                                for region in np.sort(data.region.unique())
+                            ],
+                            value="Albany",
+                            clearable=False,
+                            className="dropdown",
+                        ),
+                    ]
+                ),
+                html.Div(
+                    children=[
+                        html.Div(children="type", className="menu-title"),
+                        dcc.Dropdown(
+                            id="type-filter",
+                            options=[
+                                {"label": avocado_type, "value": avocado_type}
+                                for avocado_type in data.type.unique()
+                            ],
+                            value="organic",
+                            clearable=False,
+                            searchable=False,
+                            className="dropdown"
+                        ),
+                    ],
+                ),
+                html.Div(
+                    children=[
+                        html.Div(
+                            children="Date Range",
+                            className="menu-title"
+                        ),
+                        dcc.DatePickerRange(
+                            id="date-range",
+                            min_date_allowed=data.Date.min().date(),
+                            max_date_allowed=data.Date.max().date(),
+                            initial_visible_month=data.Date.min().date(),
+                            start_date=data.Date.min().date(),
+                            end_date=data.Date.max().date()
+                        ),
+                    ]
+                ),
+            ],
+            className="menu",
+        ),
     ]
 )
 
