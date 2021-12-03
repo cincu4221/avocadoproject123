437c8b6e0cc19b8d55ac79849b831c8c62465111
cincu4221 <75069917+cincu4221@users.noreply.github.com>
2021-12-03 10:42:12 +0900

    make it better

--- app.py
+++ app.py
@@ -1,47 +1,29 @@
+import os
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
 
-data = pd.read_csv("data/avocado.csv", index_col=0)
-data = data.query("type == 'conventional' and region == 'albany'")
-data["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
-data.sort_values("Date", inplace=True)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div(
-    children=[
-        html.H1(children="Temp Analytics"),
-        html.P(
-            children="Temp"
-        ),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": data["Date"],
-                        "y": data["AveragePrice"],
-                        "type": "lines"
-                    }
-                ],
-                "layout": {"title": "Title_1"}
-            }
-        ),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": data["Date"],
-                        "y": data["Total Volume"],
-                        "type": "lines"
-                    }
-                ],
-                "layout": {"title": "Title_2"}
-            }
-        )
-    ]
-)
+server = app.server
 
-if __name__ == "__main__":
+app.layout = html.Div([
+    html.H2('Hello World'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
+    ),
+    html.Div(id='display-value')
+])
+
+@app.callback(dash.dependencies.Output('display-value', 'children'),
+                [dash.dependencies.Input('dropdown', 'value')])
+def display_value(value):
+    return 'You have selected "{}"'.format(value)
+
+if __name__ == '__main__':
     app.run_server(debug=True)
